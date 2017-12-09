aa9082af03c48058cc9c64b3ce3e351fac34e124
uppu431@gmail.com <uppu31@gmail.com>
2017-12-09 15:17:08 +0530

    square pattren programs

---
+++ PattrenPrograms/src/com/corejava/pattrenPG/SquareNum3.java
@@ -0,0 +1,16 @@
+package com.corejava.pattrenPG;
+
+public class SquareNum3 {
+	public static void main(String[] args) {
+		int n = 3;
+		int c=9;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				System.out.print(c--);
+			}
+			System.out.println();
+
+		}
+	}
+
+}
