6ddb5fe9f6064c01de2e57b503bf73c82740fde4
uppu431@gmail.com <uppu31@gmail.com>
2017-12-09 21:23:41 +0530

    Triangle pattrens

---
+++ PattrenPrograms/src/com/corejava/numTri/pattrenPG/TriangularPattren2.java
@@ -0,0 +1,14 @@
+package com.corejava.numTri.pattrenPG;
+
+public class TriangularPattren2 {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = n; j>=i; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
